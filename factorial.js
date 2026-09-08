7ba23ee4de23561690e3dc7ec54a1c6ac1e46203
itzelx463-arch <itzelx463@gmail.com>
2026-09-08 12:41:33 -0600

    Add Factorial.js

--- factorial.js
+++ factorial.js
@@ -6,3 +6,12 @@ function factorial(n) {
 }
 
 console.log(factorial(5)); // Resultado: 120
+
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return ;
+    }
+    return n * factorial(n - 1);
+}
+
+console.log(factorial(5)); // Resultado: 120
