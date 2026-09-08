5de4bbec6680e0a2666b5d4d12f982c8157e3f11
XimenaItzel <itzelx463@gmail.com>
2026-09-08 11:47:34 -0600

    Funcion serie factorial

--- FactorialFor.js
+++ FactorialFor.js
@@ -13,3 +13,13 @@ console.log(factorialCiclo(5)); // Resultado: 120
 console.log(factorialCiclo)(5);
 console.log(factorialCiclo)(6);
 console.log(`El factorial de ${numero} es: ${factorialIterativo(numero)}`);
+
+function mostrarSerieFactorial(n) {
+    let factorial = 1;
+    for (let i = 1; i <= n; i++) {
+        factorial *= i;
+        console.log(`Factorial de ${i} (${i}!) = ${factorial}`);
+    }
+}
+
+mostrarSerieFactorial(5);
